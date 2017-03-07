a782ba4c147ccd07891228474e6eb96b310abd33
Thomas Flood <thomaspeterflood@gmail.com>
2017-03-07 12:23:35 +0000

    creation of Python project following the standard template.

---
+++ LEDarray/setup.py
@@ -0,0 +1,5 @@
+'''
+Created on 7 Mar 2017
+
+@author: thomasflood
+'''
---
+++ LEDarray/tests/test.py
@@ -0,0 +1,18 @@
+'''
+Created on 7 Mar 2017
+
+@author: thomasflood
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
